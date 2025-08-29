e07bb4ed08c20402b49992590e759082555ca068
a01710972-beep <a01710972@tec.mx>
2025-08-29 17:54:55 -0600

    Update File Archive.py

--- File Archive.py
+++ File Archive.py
@@ -1,1 +1,7 @@
-
+"""
+Proyectopython
+Simulador de examen de PISA.
+El programa realiza una serie de preguntas con semillas aleatorias
+y compara las respuesta con las del usuario para evaluarlo.
+Al final le da un marcador al usuario y muestra sus errores.
+"""
