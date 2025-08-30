86a00be4141e8b965b68707f4c7e2ba7b7327594
a01710972-beep <a01710972@tec.mx>
2025-08-29 21:16:06 -0600

    Update File Archive.py

--- File Archive.py
+++ File Archive.py
@@ -28,8 +28,11 @@ Tomando el tipo de archivo, estos se classificarán de acuerdo a este
 recibe: val_1 valor string, val_2 valor string, val_3 valor string.
 val_4 valor string, val_5 valor string
 devuelve: conjunto por palabra clave (ej. trabajo)
+Usar ifs, tipo, 
+if nombre_de_archivo contiene palabra "trabajo"
+cuando el usuario busque por palabra "trabajo"
+print(nombre_de_archivo)
 """
-
 """
 ================= Funciones de filtrado por tiempo (números) =================
 """
@@ -56,4 +59,26 @@ def evalua_resta(para_resta, val_4)
 return 5 - val_4
 
 def evalua_resta(5, val_5)
-return 5 - val_5
+return 5 - val_5}
+
+"""
+Dependiendo del valor de la resta, se clasificará el archivo como "reciente", "hace un rato",
+y "antiguo"
+En este caso: x < 3 
+devuelve: antiguo
+x == 3
+devuelve: hace un rato
+x > 3
+devuelve: reciente
+"""
+
+if resultado_de_resta = < 3
+print("antiguo2")
+if resultado_de_resta = ==3
+print("hace un rato")
+if resultado_de_resta = > 3
+print("reciente")
+
+"""
+============ parte principal ===========
+"""
