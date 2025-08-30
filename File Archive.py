144342aa7ffafee63b9aa0ad13d10fcb41c7de3c
a01710972-beep <a01710972@tec.mx>
2025-08-29 20:23:21 -0600

    Update File Archive.py
    No se que hice pero no me dejaba guardar mi avance, tuve que copiar y pegar al abrirlo de nuevo para poderlo guardar, tengo video de esto

--- File Archive.py
+++ File Archive.py
@@ -1,7 +1,48 @@
 """
 Proyecto python
 Filtrador de Archivos.
-El programa realiza una serie de preguntas con semillas aleatorias
-y compara las respuesta con las del usuario para evaluarlo.
-Al final le da un marcador al usuario y muestra sus errores.
+El programa ordena los archivos por diferentes tipos de métodos, ya sea por orden alfabético, 
+tema de archivo y el archivo más reciente al más antiguo.
+Se le pedirá al usuario que ponga el nombre de sus archivos (máximo 5) con el nombre de su elección 
+siempre y cuando este tenga en su terminación el tema de este (ej. escuela, trabajo, etc.).
+Al final, el usuario debería poder buscar sus archivos previamente guardados por cualquiera
+de los 3 métodos existentes. 
 """
+#bibliotecas
+
+"""
+================= Funciones de filtrado por metodo alfabetico =================
+"""
+
+
+"""
+================= Funciones de filtrado por tema =================
+"""
+
+
+"""
+================= Funciones de filtrado por tiempo =================
+"""
+
+def evalua_resta(5, val_1)
+"""
+A los datos que el usuario introduzca se les asigna un valor numérico.
+(uso de operadores)
+recibe: val_1 valor númerico, val_2 valor númerico, val_3 valor númerico,
+val_4 valor númerico, val_5 valor númerico
+resta 5 a cada valor
+devuelve: los valores ordenados de acuerdo a que tan grande es el número final
+"""
+return 5 - val_1
+
+def evalua_resta(5, val_2)
+return 5 - val_2
+
+def evalua_resta(5, val_3)
+return 5 - val_3
+
+def evalua_resta(5, val_4)
+return 5 - val_4
+
+def evalua_resta(5, val_5)
+return 5 - val_5
