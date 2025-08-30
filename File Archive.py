161071d693382e86c3d1149535e1795f7c85ae87
a01710972-beep <a01710972@tec.mx>
2025-08-29 18:08:43 -0600

    Update File Archive.py

--- File Archive.py
+++ File Archive.py
@@ -1,6 +1,6 @@
 """
-Proyectopython
-Simulador de examen de PISA.
+Proyecto python
+Filtrador de Archivos.
 El programa realiza una serie de preguntas con semillas aleatorias
 y compara las respuesta con las del usuario para evaluarlo.
 Al final le da un marcador al usuario y muestra sus errores.
